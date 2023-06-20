f8776ec25b0e34ac892cd25e4c1d208e094d2867
Berest-Dmitry <67856343+Berest-Dmitry@users.noreply.github.com>
2023-06-20 09:59:37 +0300

    first commit

---
+++ test-vue-proj1/src/views/AboutView.vue
@@ -0,0 +1,105 @@
+<script setup>
+  import {ref, reactive} from 'vue'
+    const WelcomeMsg = ref('Hello from test project!');
+    const UserMsg = ref('');
+    const LastMsg = ref('');
+    const count = ref(0);
+    const ideasCount = ref('');
+    const colorClass = ref('');
+    const colors = [
+      "red",
+      "green",
+      "blue"
+    ]
+    
+
+    function submit(){
+      alert("You've written: " + UserMsg.value);
+      // UserMsg.value = '';
+    }
+
+    function SaveLastMessage(){
+      LastMsg.value = UserMsg.value;
+      UserMsg.value = '';
+    }
+    function ClearLastMessage(){
+      if(LastMsg.value){
+        LastMsg.value = '';
+        count.value++;
+        ideasCount.value = "Total ideas count is " + count.value;
+      }
+      
+    }
+
+    function pickColor(){
+      let colorIndex = getRndInteger(0, 2);
+      colorClass.value = colors[colorIndex];
+    }
+    function getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+</script>
+
+<template>
+  <div class="row about" >
+    <h1 style="display: block; height: 150px;">{{WelcomeMsg}}</h1>
+    <div class="row">
+      <div class="input-form col-6">
+        <input type="text" class="user-input" placeholder="type something..." v-model="UserMsg">
+        
+      </div>
+      <div class="col-6 row" style="margin-left: 10px;">
+        <button type="button" @click="submit()" class="vue-button col-4"> Send </button>
+        <button type="button" @click="SaveLastMessage()" class="vue-button col-4">Save Message</button>
+        <button type="button" @click="ClearLastMessage()" class="vue-button col-4">Clear Message</button>
+      </div>
+    </div>
+    <div class="row">
+      <span class="vue-span" :class="colorClass" :title="ideasCount" @click="pickColor()">Your thoughts: {{LastMsg}}</span>
+    </div>
+  </div>
+  
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    /*min-height: 100vh;*/
+    display: flex;
+    align-items: center;
+  }
+  .user-input{
+    text-decoration: underline;
+    font-size: 25px;
+    font-style: italic;
+  }
+  .input-form{
+    border: 1pt black solid;
+    border-radius: 5px;
+    background-color: bisque;
+  }
+  .vue-button{
+    background-color: lightgreen;
+    color: whitesmoke;
+    border-color: white;
+    border-radius: 3px;  
+  }
+  .vue-span{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-size: large;
+    font-weight: 600;
+    font-family: 'Times New Roman', Times, serif;
+    background-color: cadetblue;
+  }
+  .red{
+    color: red;
+  }
+  .green{
+    color: green;
+  }
+  .blue{
+    color: blue;
+  }
+}
+</style>
