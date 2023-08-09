90a754335abf155128b48cded78e18aa24499128
Berest-Dmitry <dmitrijberest@gmail.com>
2023-08-09 15:22:27 +0300

    исправление навигационной панели (стили); удаление компонента HelloWorld

--- test-vue-proj1/src/App.vue
+++ test-vue-proj1/src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -37,7 +37,8 @@ nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+  /* margin-top: 2rem; */
+  background-color: #42b883;
 }
 
 nav a.router-link-exact-active {
@@ -52,6 +53,7 @@ nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
+  color: white;
 }
 
 nav a:first-of-type {
@@ -71,17 +73,22 @@ nav a:first-of-type {
 
   header .wrapper {
     display: flex;
-    place-items: flex-start;
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%);
+    place-items: center start;
     flex-wrap: wrap;
+    width: 100%;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
+    /* text-align: left; */
+    /* margin-left: -1rem; */
     font-size: 1rem;
 
     padding: 1rem 0;
-    margin-top: 1rem;
+    /* margin-top: 1rem; */
   }
 
   h1{
